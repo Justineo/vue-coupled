d4221289314b3259fa5dcabba2f096123922771d
Justineo <justice360@gmail.com>
2022-02-07 14:08:49 +0800

    chore: update build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
-import { fileURLToPath, URL } from 'url'
-
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'VueCoupled',
+      fileName: (format) => `vue-coupled.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
     },
   },
 })
