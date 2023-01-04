e9dd3a1acd9d3ad923e322d148414ed33acbd6c3
Justineo <justice360@gmail.com>
2023-01-04 23:37:11 +0800

    chore: update deps versions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,10 +6,13 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier',
   ],
-  env: {
-    'vue/setup-compiler-macros': true,
+  parserOptions: {
+    ecmaVersion: 'latest',
   },
+  env: {
+    'vue/setup-compiler-macros': true
+  }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import { resolve } from 'path'
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [vue()],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
       name: 'VueCoupled',
       fileName: (format) => `vue-coupled.${format}.js`,
     },
